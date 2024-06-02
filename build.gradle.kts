16d27eedc04b5dda861e2d079c89ecdc326cff28
vonlinee <vonlinee@163.com>
2024-06-02 17:15:35 +0800

    build:升级Intellij Platform SDK版本及相关Gradle插件版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,50 +9,49 @@ buildscript {
         maven { url = uri("https://dl.bintray.com/jetbrains/intellij-third-party-dependencies/") }
     }
     dependencies {
-        classpath("org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.7.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.17.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
 
 plugins {
     java
-    kotlin("jvm") version "1.4.32"
-    id("org.jetbrains.intellij") version "0.7.2"
+    kotlin("jvm") version "1.9.20"
+    id("org.jetbrains.intellij") version "1.14.0"
 }
 
-java(Action {
+java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-})
+}
+
+kotlin {
+    jvmToolchain(8)  // 生成的字节码的目标版本
+}
 
 // https://github.com/JetBrains/gradle-intellij-plugin/
 // http://www.jetbrains.org/intellij/sdk/docs/tutorials/build_system/prerequisites.html
-intellij(Action {
+intellij {
     version = "2020.2"
+    type = "IU" // 企业版 type="IC"  // 社区版
 
-//    type="IC"  // 社区版
-//    setPlugins(arrayOf("java")) //Bundled plugin dependencies
-    type = "IU" // 企业版
-    setPlugins(
-        "java",
-        "Kotlin",
-        "IntelliLang",
-        "Spring",
-        "SpringBoot",
-        "DatabaseTools"
-    ) //Bundled plugin dependencies
+    // Bundled plugin dependencies
+    plugins.set(
+        listOf(
+            "java",
+            "Kotlin",
+            "IntelliLang",
+            "Spring",
+            "SpringBoot",
+            "DatabaseTools"
+        )
+    )
 
     pluginName = "MybatisX"
-    sandboxDirectory = "${rootProject.rootDir}/idea-sandbox"
-
+    sandboxDir = "${rootProject.rootDir}/idea-sandbox"
     updateSinceUntilBuild = false
-    isDownloadSources = true
-})
-
-//publishPlugin {
-//    username = project.hasProperty("publishUsername") ? project.property("publishUsername") : ""
-//    password = project.hasProperty("publishPassword") ? project.property("publishPassword") : ""
-//}
+    downloadSources = true
+}
 
 // 各种版本去这里找
 // https://www.jetbrains.com/intellij-repository/releases
@@ -62,7 +61,6 @@ version = "1.5.2"
 
 repositories {
     mavenLocal()
-    jcenter()
     mavenCentral()
 }
 
@@ -73,11 +71,22 @@ dependencies {
     implementation("org.mybatis:mybatis:3.5.16")
     implementation("org.freemarker:freemarker:2.3.30")
     implementation("com.itranswarp:compiler:1.0")
-    testCompile("junit:junit:4.13.1")
-    testCompile("commons-io:commons-io:2.8.0")
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("commons-io:commons-io:2.8.0")
     compileOnly("org.projectlombok:lombok:1.18.0")
+
+    implementation(files("../libs/tools.jar"))
+
+    // 解决 lombok 不生效问题
+    // 还需在Build,Execution,Deployment -> Compiler -> Annotation Processor开启注解处理器
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+tasks.test {
+    // 跳过单元测试任务
+    enabled = false
 }
