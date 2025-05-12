c109e7405ec2963951e195a85e4c82e4cf03040f
vonlinee <vonlinee@163.com>
2025-05-13 01:45:53 +0800

    fix:Sql预览XML语法高亮

--- mybatisx-plugin/src/main/java/com/baomidou/mybatisx/plugin/intention/MapperStatementEditor.java
+++ mybatisx-plugin/src/main/java/com/baomidou/mybatisx/plugin/intention/MapperStatementEditor.java
@@ -59,14 +59,14 @@ public class MapperStatementEditor extends LanguageTextField {
     editor.setVerticalScrollbarVisible(true);
 
     EditorSettings editorSettings = editor.getSettings();
-    editorSettings.setVirtualSpace(false);
-    editorSettings.setLineMarkerAreaShown(false);
-    editorSettings.setIndentGuidesShown(false);
-    editorSettings.setLineNumbersShown(false);
-    editorSettings.setFoldingOutlineShown(false);
-    editorSettings.setAdditionalColumnsCount(3);
-    editorSettings.setAdditionalLinesCount(3);
-    editorSettings.setCaretRowShown(false);
+//    editorSettings.setVirtualSpace(false);
+//    editorSettings.setLineMarkerAreaShown(false);
+//    editorSettings.setIndentGuidesShown(false);
+//    editorSettings.setLineNumbersShown(false);
+//    editorSettings.setFoldingOutlineShown(false);
+//    editorSettings.setAdditionalColumnsCount(3);
+//    editorSettings.setAdditionalLinesCount(3);
+//    editorSettings.setCaretRowShown(false);
 
     editor.getDocument().addDocumentListener(new DocumentListener() {
       @Override
